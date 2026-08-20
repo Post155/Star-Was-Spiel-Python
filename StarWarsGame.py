f98b8b23a28bc1ca89fbaa919993f49b5d2d1e6d
Jonas Schröder <mail@jonasschroeder.de>
2026-08-20 13:03:37 +0200

    Refactor player class and improve movement logic

--- StarWarsGame.py
+++ StarWarsGame.py
@@ -1,90 +1,117 @@
-import pygame, random, math
+import pygame,sys
 pygame.init()
 
-# Bilder laden
-x_wing_img = pygame.image.load("Pixelarts/X_Wing.png")
+# Fenster
+width = 800
+height = 600
 
-"""
-torpedo_img = pygame.image.load("Pixelarts/Torpedo.png")
-
-explosion_img = pygame.image.load("Pixelarts/Explosion.png")
-
-asteroid_images = [
-    pygame.image.load("Pixelarts/Astroids/frame_01"),
-    pygame.image.load("Pixelarts/Astroids/frame_02"),
-    pygame.image.load("Pixelarts/Astroids/frame_03"),
-    pygame.image.load("Pixelarts/Astroids/frame_04"),
-    pygame.image.load("Pixelarts/Astroids/frame_05"),
-    pygame.image.load("Pixelarts/Astroids/frame_06"),
-    pygame.image.load("Pixelarts/Astroids/frame_07"),
-    pygame.image.load("Pixelarts/Astroids/frame_08"),
-    pygame.image.load("Pixelarts/Astroids/frame_09"),
-    pygame.image.load("Pixelarts/Astroids/frame_10"),
-    pygame.image.load("Pixelarts/Astroids/frame_11"),
-]
-"""
-
-# Spielfläche
-size = width, height = 1280, 720
-
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Star Wars")
 
 clock = pygame.time.Clock()
 
 # Farben
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
-# Spieler
-original_width, original_height = x_wing_img.get_size()
+# Bild laden
+x_wing_img = pygame.image.load(
+    "Pixelarts/x_wing.png"
+).convert_alpha()
 
-PLAYER_WIDTH = original_width // 4
-PLAYER_HEIGHT = original_height // 4
 
-player_img = pygame.transform.scale(
+class Spieler:
+    def __init__(self, bild, fenster_breite, fenster_hoehe):
+        self.show_hitbox = False
+
+        # Bild auf 1/4 verkleinern
+        self.image = pygame.transform.scale_by(bild, 0.25)
+
+        self.width, self.height = self.image.get_size()
+
+        # Startposition
+        self.x = fenster_breite // 2 - self.width // 2
+        self.y = fenster_hoehe - self.height - 20
+
+        self.speed = 5
+
+    def move_left(self):
+        self.x -= self.speed
+
+        if self.x < 0:
+            self.x = 0
+
+    def move_right(self):
+        self.x += self.speed
+
+        if self.x > width - self.width:
+            self.x = width - self.width
+
+    def get_rect(self):
+        return pygame.Rect(
+            self.x,
+            self.y,
+            self.width,
+            self.height
+        )
+
+    def draw(self, screen):
+
+        screen.blit(
+            self.image,
+            (self.x, self.y)
+        )
+
+        # Hitbox zeichnen
+    def draw(self, screen):
+
+        screen.blit(
+            self.image,
+            (self.x, self.y)
+        )
+
+        if self.show_hitbox:
+            pygame.draw.rect(
+                screen,
+                RED,
+                self.get_rect(),
+                2
+            )
+
+
+# Spieler erzeugen
+spieler = Spieler(
     x_wing_img,
-    (PLAYER_WIDTH, PLAYER_HEIGHT)
+    width,
+    height
 )
 
-player_x = width // 2 - PLAYER_WIDTH // 2
-player_y = height - PLAYER_HEIGHT - 20
-
-speed = 10
-
-# Spiel Logic
 running = True
 
 while running:
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
-    # Tastatur abfragen
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        player_x -= speed
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        spieler.move_left()
 
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        player_x += speed
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        spieler.move_right()
 
-    # Im Fenster halten
-    if player_x < 0:
-        player_x = 0
+    if keys[pygame.K_ESCAPE]:
+        running = False
 
-    if player_x > width - PLAYER_WIDTH:
-        player_x = width - PLAYER_WIDTH
+    if keys[pygame.K_h]:
+        spieler.show_hitbox = not spieler.show_hitbox
 
-    # Zeichnen
     screen.fill(BLACK)
 
-    screen.blit(player_img, (player_x, player_y))
-
-    # Hitbox
-    if keys[pygame.K_h]:
-        picture_rect = pygame.Rect(player_x, player_y, PLAYER_WIDTH, PLAYER_HEIGHT)
-        pygame.draw.rect(screen, (255, 0, 0), picture_rect, 2)
+    spieler.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
